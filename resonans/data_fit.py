35aaeda12d3f26aebf6453215febf8e60fdfac77
Bryukvina Darja <100697083+Bryukvina@users.noreply.github.com>
2023-09-08 15:27:10 +0300

    Update data_fit.py

--- resonans/data_fit.py
+++ resonans/data_fit.py
@@ -1,3 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import solve_ivp
+import cmath
+
+
 #function, that takes parametrs and delta_frequensy list and return list of transmitted wave power points
 
 def theor(x, do, dc, g):
@@ -7,12 +13,20 @@ def theor(x, do, dc, g):
     y = np.array(y)
     return y
 
+#reading real data
+
+file = open("C:\Job\RQC\experiment_data\modified_data.txt", "r")
+s = file.readline()
+channel1 = []
+channel2 = []
+channel3 = []
+times = []
+
 #data from file with processing
 
 s = file.readline()
 while s != "":
     t, c1, c2, c3 = map(float, s.split(", "))
-    print(t)
     times.append(t)
     channel1.append(c1)
     channel2.append(c2)
@@ -26,19 +40,31 @@ count = 0
 for i in range(1, len(channel2) - 1):
     if channel2[i - 1] < channel2[i] and channel2[i + 1] < channel2[i]:
         count += 1
-#constant dw = ....
-
+        
+dw = 102000000
 dt = (max(times) - min(times)) / count
-freq = dw / dt
-
-#draw grafix with fitting data. parameters were had already piked up
-
-plt.figure(figsize=(6, 3))
-plt.plot(freq, theor(freq, 1 / 30000000000, 5/ 30000000000, 5/ 30000000000))
-plt.plot(freq + 0.0000000002, (channel3 - min(channel3)) /(8.4099E-01 - min(channel3)))
-plt.show()
+speed = dw / dt
+freq = times * speed
 
 #parameters
-do = 3.3333333333333335e-11
-dc = 1.6666666666666666e-10
-g = 1.6666666666666666e-10
+do = 2.5 * 10000000000
+dc = 7.1 * 10000000000
+g = 4.9 * 10000000000
+
+#draw grafix with fitting data. parameters were had already piked up (part of data with resonanse)
+
+theor_list = theor(freq, d0, dc, g)
+freq_prac = freq + 0.85 * 10 ** (11)
+prac_norm = (channel3 - min(channel3)) /(8.4099E-01 - min(channel3))
+
+plt.figure(figsize=(6, 3))
+plt.plot(freq[len(freq) // 3:len(freq) // 2], theor_list[len(freq) // 3:len(freq) //2],  label="theoretical_data")
+plt.plot(freq_prac[len(freq) // 3:len(freq)  // 2], prac_norm[len(freq) // 3:len(freq) // 2], label="experiment_data")
+plt.legend()
+plt.show()
+
+plt.figure(figsize=(6, 3))
+plt.plot(freq, theor_list, label="theoretical_data")
+plt.plot(freq_prac, prac_norm, label="experiment_data")
+plt.legend()
+plt.show()
